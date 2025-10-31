76ba47b1252c82d23a96466c64e8256249ad5883
avni955587 <avni955587@gmail.com>
2025-10-31 13:33:35 +0530

    done

---
+++ javascipt/jS/palinString.js
@@ -0,0 +1,9 @@
+// Write, Edit and Run your Javascript code using JS Online Compilerlet word ="Javascipt"
+let word ="racecar"
+let av=word
+let revStr = word.split("").reverse().join("")
+ 
+if (revStr==word)
+console.log("Palin")
+else
+console.log("Not")
